e4957f6d3f3f23aee586e658399bb2a7379d92ab
Victor Gabriel Alcantara <67282866+victorgalcantara@users.noreply.github.com>
2022-01-18 11:31:27 -0300

    update

--- JavaScript/challenge-1/script.js
+++ JavaScript/challenge-1/script.js
@@ -1,3 +1,4 @@
+///                   DESAFIO 1 - ESTRUTURA
 // crie uma variável que receba uma string
 let firstName = "Victor"
 let lastName  = "Alcantara"
@@ -24,6 +25,7 @@ console.log(quartaFeira)
 // Exiba no console o tipo de dado da váriavel quartaFeira;
 console.log(typeof quartaFeira)
 
+///                   DESAFIO 2 - LÓGICA
 //1 - Crie uma condição que exiba uma mensagem no console 
 // se a idade for maior que 18
 if(age > 18){
@@ -33,7 +35,7 @@ if(age > 18){
 //2 - Crie uma condição que exiba uma mensagem no console 
 // se você for maior de idade E a condição humana seja true
 var human = true
-if(age > 18 & human){
+if(age > 18 && human){
     console.log("Humano e maior que 18 anos")
 } else{""}
 
@@ -60,3 +62,39 @@ if(firstLetter == "V"){
 if(lastName.length > 6 || firstLetter == "E"){
     console.log("O sobrenome possui mais de 6 letras ou o nome começa com a letra E")
 }
+
+///                   DESAFIO 3 - LOOPS
+
+// 2 - Criar um loop que conte de 10 até 1 usando WHILE
+let lastN = 10
+console.log(lastN,"to 1 \n Code by while")
+while(lastN >= 1){
+console.log("number ",lastN)
+    lastN = lastN - 1
+}
+
+// 3 - Criar um loop que conte todos os números ímpares de 1 até 100
+// odd numbers from 1 to 100 with WHILE and FOR
+let firstN2 = 1
+console.log("Odd numbers from 1 to 100 \n Code by while")
+while(100 >= firstN2){
+    console.log("number ",firstN2)
+    firstN2=firstN2+2
+}
+console.log("Odd numbers from 1 to 100 \n Code by for")
+// com for
+for(let i = 1;i <= 100;i+=2)
+    console.log("number ",i)
+
+// 4 - Criar um loop que conte todos os números pares de 0 a 100
+// even numbers from 1 to 100 with WHILE and FOR
+let firstN = 2
+console.log("Even numbers from 1 to 100 \n Code by while")
+while(100 >= firstN){
+   console.log("number ",firstN)
+   firstN=firstN+2
+}
+
+console.log("Even numbers from 1 to 100 \n Code by for")
+for(let i = 2;i <= 100;i=i+2)
+   console.log("number ",i)
